f0e7b438024cb7f64ca11befc956f85fcaa38cd1
RelinDmitrii <relin-dmitrii@yandex.ru>
2020-11-19 21:09:13 +0300

    hw1

---
+++ src/lesson5HW_2course/Main.java
@@ -0,0 +1,9 @@
+package lesson5HW_2course;
+
+public class Main {
+    public static void main(String[] args) {
+        MethodsForArrays a = new MethodsForArrays();
+        a.startOneThread();
+        a.startTwoThreads();
+    }
+}
