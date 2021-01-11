749ac06243d0308815fef31f1d9bcebc26494e68
RelinDmitrii <relin-dmitrii@yandex.ru>
2021-01-11 21:11:33 +0300

    hw1

---
+++ src/lesson5HW_3course/Tunnel.java
@@ -0,0 +1,27 @@
+package lesson5HW_3course;
+
+public class Tunnel extends Stage {
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                Main.tunnel.acquire();
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+                Main.tunnel.release();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
